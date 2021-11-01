0712ec27c910444009eeb26d163b9218395d43e8
sawa-it <42475390+sawa-it@users.noreply.github.com>
2021-11-01 10:04:00 +0900

    作成対象ディレクトリの名称変更

--- scv.py
+++ scv.py
@@ -16,7 +16,7 @@ for file in json_files:
 
 # コミュニケーションの解析
 commication = []
-json_files = glob.glob("./logs/ex-herokura/*.json") # 特定のチャンネルを確認する時はここを変更する
+json_files = glob.glob("./logs/*/*.json") # 特定のチャンネルを確認する時はここを変更する
 pattern = '<@(.*?)>' # メンションのパターン
 graph = Graph(format='svg', engine='sfdp')
 path = []
